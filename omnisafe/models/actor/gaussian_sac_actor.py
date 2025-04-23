0b70f5228f243cf7b94e42268834f87eec5c067f
asharamanujam <103427149+asharamanujam@users.noreply.github.com>
2025-04-23 11:52:09 -0400

    Update gaussian_sac_actor.py

--- omnisafe/models/actor/gaussian_sac_actor.py
+++ omnisafe/models/actor/gaussian_sac_actor.py
@@ -51,6 +51,7 @@ class GaussianSACActor(Actor):
         hidden_sizes: list[int],
         activation: Activation = 'relu',
         weight_initialization_mode: InitFunction = 'kaiming_uniform',
+        output_activation: Activation = 'relu',
     ) -> None:
         """Initialize an instance of :class:`GaussianSACActor`."""
         super().__init__(obs_space, act_space, hidden_sizes, activation, weight_initialization_mode)
@@ -58,7 +59,7 @@ class GaussianSACActor(Actor):
         self.net: nn.Module = build_mlp_network(
             sizes=[self._obs_dim, *self._hidden_sizes, self._act_dim * 2],
             activation=activation,
-            weight_initialization_mode=weight_initialization_mode,
+            weight_initialization_mode=weight_initialization_mode,output_activation = output_activation
         )
 
         self._current_raw_action: torch.Tensor | None = None
