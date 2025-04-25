f28b5c5ef9553aabeb0e5bab586d48a2c0b28e95
asharamanujam <103427149+asharamanujam@users.noreply.github.com>
2025-04-25 12:20:54 -0400

    Update ddpg.py

--- omnisafe/algorithms/off_policy/ddpg.py
+++ omnisafe/algorithms/off_policy/ddpg.py
@@ -336,7 +336,7 @@ class DDPG(BaseAlgo):
             self._logger.dump_tabular()
 
             # save model to disk
-            if (epoch + 1) % self._cfgs.logger_cfgs.save_model_freq == 0:
+            if (epoch + 1) % self._cfgs.logger_cfgs.save_model_freq == 0 or (epoch + 1) == self._cfgs.train_cfgs.epochs:
                 self._logger.torch_save()
 
         ep_ret = self._logger.get_stats('Metrics/EpRet')[0]
